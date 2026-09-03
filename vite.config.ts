35cddf3949126cd5c2bac5fbdff9e0c03e361d44
Eitan Mosenkis <eitan@coveredhealth.ai>
2026-09-03 12:23:53 +0300

    chore: run frontend and backend with paseo

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:8000', '/uploads': 'http://localhost:8000' } },
+  server: { proxy: {
+    '/api': process.env.VITE_BACKEND_URL ?? 'http://localhost:8000',
+    '/uploads': process.env.VITE_BACKEND_URL ?? 'http://localhost:8000',
+  } },
   test: { environment: 'jsdom', setupFiles: './src/test-setup.ts', include: ['src/**/*.test.tsx'] },
 })
